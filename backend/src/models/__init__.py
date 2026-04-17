dd1cd6d1f431129737e9485dd52ff0ab794c4524
Sofia <sofya.vedenina2005@gmail.com>
2026-04-17 17:01:29 +0300

    import changes

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,5 +1,6 @@
 from .base import Base
 from .checklist_category import ChecklistCategory
+from .checklist_checklist_category import ChecklistChecklistCategory
 from .checklist_item import ChecklistItem
 from .checklist import Checklist
 from .comment import Comment
@@ -10,5 +11,6 @@ from .user_progress import UserProgress
 from .user import User
 
 
-__all__ = ["Base", "ChecklistCategory", "ChecklistItem", "Checklist", "Comment", 
-           "Favorite", "Like", "Post", "UserProgress", "User"]
+__all__ = ["Base", "ChecklistCategory", "ChecklistChecklistCategory", 
+           "ChecklistItem", "Checklist", "Comment", "Favorite", 
+           "Like", "Post", "UserProgress", "User"]
