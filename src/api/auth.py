c72b77b7dad248f78e9b68353fd084d6de37b37d
Софья <sofya.vedenina.2005@gmail.com>
2026-05-24 23:01:30 +0300

    Update auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -34,7 +34,7 @@ def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(get_db)
 ):
-    user = db.query(User).filter(User.email == form_data.username).first()
+    user = db.query(User).filter(User.username == form_data.username).first()
     if not user or not verify_password(form_data.password, user.password_hash):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
