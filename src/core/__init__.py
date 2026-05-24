80d61415ddb62c88bacff4311d8d8b1a90136330
Софья <sofya.vedenina.2005@gmail.com>
2026-05-24 22:22:37 +0300

    Update __init__.py

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,5 +1,5 @@
 from .config import settings
 from .security import verify_password, get_password_hash, create_access_token, create_refresh_token, decode_token
-# from .dependencies import get_current_user
+from .dependencies import get_current_user
 
 __all__ = ["settings", "verify_password", "get_password_hash", "create_access_token", "create_refresh_token", "decode_token"]
