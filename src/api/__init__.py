c38268b245f62f8c211e41ab572af2e4113324a2
Софья <sofya.vedenina.2005@gmail.com>
2026-05-25 10:22:56 +0000

    оставила только нужную папку во фронте

--- src/api/__init__.py
+++
@@ -1,5 +0,0 @@
-from .auth import register, login, refresh_token
-from .user import get_current_user_info
-
-
-__all__ = ["register", "login", "refresh_token", "get_current_user_info"]
--- src/database/__init__.py
+++
@@ -1,5 +0,0 @@
-from .engine import engine, create_db_and_tables
-from .session import SessionLocal, get_db
-
-
-__all__ = ["engine", "create_db_and_tables", "SessionLocal", "get_db"]
